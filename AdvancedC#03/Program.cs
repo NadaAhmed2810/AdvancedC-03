37f2f9b09748983abf4382d392212004d9d7ca5d
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-25 16:56:30 +0200

    What is delegate

--- AdvancedC#03/Program.cs
+++ AdvancedC#03/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What is  Delegate
+            //Delegate is a C# feature [C# 2.0 ]
+            //Has a 2 Usages
+            //1. Functional programming[function as parameter, function return function,storage function in variable]
+            //2. Event_Driven programming
+            //delegate transform to Class  in CLR 
+
+            #endregion
         }
     }
 }
