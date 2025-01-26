2c8b122610d5b242876ae4f6b24285e64c2b79f3
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-26 12:06:18 +0200

    a. User Defined Delegate Datatype

---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region a. User Defined Delegate Datatype
+            //BookFunc func01=BookFunctions.GetTitle;
+
+            //string[] Authors = ["Nada", "Noura", "Nour", "Radwa"];
+            //Book book=new Book("2002206","Advanced Software",Authors,DateTime.Now,500);
+            //Console.WriteLine(func01.Invoke(book));
+            //func01 = BookFunctions.GetPrice;
+            //Console.WriteLine(func01.Invoke(book));
+            //func01= BookFunctions.GetAuthors;
+            //Console.WriteLine(func01.Invoke(book)); 
+            #endregion
+
+
+
+        }
+    }
+}
